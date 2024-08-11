7d6703f2799ebd5cf2d815c764301628c9dda7ed
Tong Cheng <cheng.tong@outlook.de>
2024-08-11 16:11:20 +0200

    Fix error

--- src/js/content.js
+++ src/js/content.js
@@ -5,7 +5,7 @@ function removePipAttribute(video) {
 }
 
 function createPipControl() {
-    const pipIcon = '<svg xmlns="http://www.w3.org/2000/svg" height="20px" viewBox="0 -960 960 960" width="20px" fill="#e8eaed"><path d="M96-480v-72h165L71-743l50-50 191 190v-165h72v288H96Zm72 288q-29.7 0-50.85-21.15Q96-234.3 96-264v-144h72v144h336v72H168Zm624-264v-240H456v-72h336q29.7 0 50.85 21.15Q864-725.7 864-696v240h-72ZM576-192v-192h288v192H576Z"/></svg>';
+    const pipIcon = `<svg xmlns="http://www.w3.org/2000/svg" height="20px" viewBox="0 -960 960 960" width="20px" fill="#e8eaed"><path d="M96-480v-72h165L71-743l50-50 191 190v-165h72v288H96Zm72 288q-29.7 0-50.85-21.15Q96-234.3 96-264v-144h72v144h336v72H168Zm624-264v-240H456v-72h336q29.7 0 50.85 21.15Q864-725.7 864-696v240h-72ZM576-192v-192h288v192H576Z"/></svg>`;
     const pipControl = document.createElement('div');
     pipControl.setAttribute('id', 'pipControl');
     pipControl.classList.add('pip-control');
